9d82cf20324879a5f46d6bad32a4387b15d377f2
amerrez <arez@hawk.iit.edu>
2018-12-02 16:57:26 -0800

    features and target correlation analysis

--- src/titanic.py
+++ src/titanic.py
@@ -4,15 +4,35 @@ import seaborn as sns
 import numpy as np 
 
 
-df = pd.read_csv("/Users/amerrez/CS286/Titanic/train.csv")
+df = pd.read_csv("/Users/amerrez/CS286/Titanic/train_split.csv")
 # ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp','Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
 
 cols = df.columns
+selectedColumns = ['Survived', 'Age', 'SibSp', 'Parch', 'Fare','Sex_female','Sex_male','Embarked_C','Embarked_Q','Embarked_S','Pclass_1','Pclass_2','Pclass_3']
 
-cm = np.corrcoef(df[['Survived','Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']].values.T) 
-sns.set(font_scale=1.5) 
+cm = np.corrcoef(df[['Survived', 'Age', 'SibSp', 'Parch', 'Fare','Sex_female','Sex_male','Embarked_C','Embarked_Q','Embarked_S','Pclass_1','Pclass_2','Pclass_3']].values.T) 
+sns.set(font_scale=1.5)
+
+threshold = 0.8
+## print the features corelated with the target :
+print("The features corelated with the target based on threshold " + str(threshold))
+for rowIndex in range(len(cm)):
+    corrIndex = 0 # the target index
+    if rowIndex != corrIndex:
+        if (cm[rowIndex][corrIndex] > threshold or cm[rowIndex][corrIndex] < -threshold) :
+            print(selectedColumns[rowIndex] + " and " + selectedColumns[corrIndex] + " are dependent")
+print("\n")
+
+## print the features corelated with each others
+print("The features corelated with each others based on threshold " + str(threshold))
+for rowIndex in range(1, len(cm)):
+    for corrIndex in range(1, len(cm[rowIndex])):
+        if rowIndex != corrIndex:
+            if (cm[rowIndex][corrIndex] > threshold or cm[rowIndex][corrIndex] < -threshold):
+                print(selectedColumns[rowIndex] + " and " + selectedColumns[corrIndex] + " are dependent")
+            
+## plot the Heatmap 
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 15}, yticklabels=cols, xticklabels=cols) 
-
 plt.show()
 
 
